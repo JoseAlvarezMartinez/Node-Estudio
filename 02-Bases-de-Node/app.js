04aa769fbdde74f105beaa7bb7246119d90b634f
José Alvarez <97672139+JoseAlvarezMartinez@users.noreply.github.com>
2023-06-14 09:44:07 -0300

    -- requiere --

--- 02-Bases-de-Node/app.js
+++ 02-Bases-de-Node/app.js
@@ -1,7 +1,20 @@
-console.clear()
-console.log("============")
-console.log("Tabla del: 5")
-console.log("============")
-for(let i = 0; i <= 10; i++){
-    console.log(`5 x ${i} = ${5 * i}`)
+const fs = require("fs");
+
+const readline = require("readline");
+
+
+let valor = 2
+console.clear();
+console.log("============");
+console.log(`Tabla del: ${valor}`);
+console.log("============");
+
+
+let salida = "";
+for (let i = 0; i <= 10; i++) {
+  salida += `${valor} x ${i} = ${valor * i} \n`;
 }
+fs.writeFile(`tabla-${valor}.txt`, salida, (err) => {
+  if (err) throw err;
+  console.log(`El archivo tabla-${valor}.txt se creo correctamente\n`,salida );
+});
